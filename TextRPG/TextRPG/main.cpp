dcadbf9d85fa6231360b20ccaa00a921d8a6aee1
Sombraa <seodosa5@gmail.com>
2018-07-15 23:28:55 +0900

    main skeleton2

--- TextRPG/TextRPG/main.cpp
+++ TextRPG/TextRPG/main.cpp
@@ -2,8 +2,36 @@
 
 using namespace std;
 
+enum MAIN_MENU
+{
+	MM_NONE,
+	MM_MAP,
+	MM_STORE,
+	MM_INVENTORY,
+	MM_EXIT
+
+};
+
 int main() 
 {
+	while (true)
+	{
+		system("cls");
+		cout << "1. Map" << endl;
+		cout << "2. Store" << endl;
+		cout << "3. Inventory" << endl;
+		cout << "4. Exit" << endl;
+
+		int iMenu;
+		cin >> iMenu;
+
+		if (cin.fail()) 
+		{
+			cin.clear();
+			cin.ignore(1024, '\n');
+			continue;
+		}
+	}
 
 	return 0;
 }
